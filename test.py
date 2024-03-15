63ed1424ac6be312b4ed614805e90fbae392bff4
FeelKe <felep.jopa@mail.ru>
2024-03-15 21:19:47 +0500

    Асинхронность

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 import asyncio
+import logging
+from urllib.parse import unquote
+
+import mysql.connector
 from pyppeteer import launch
 from pyppeteer.errors import PageError
-from urllib.parse import unquote
-import mysql.connector
+
 import pathes
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+main_tasks_finished = False
 
 
 async def get_element_text(page, xpath):
@@ -13,7 +22,7 @@ async def get_element_text(page, xpath):
             return element ? element.textContent : '';
         }''', xpath)
     except Exception as e:
-        print(e)
+        logger.error(e)
         return False
 
 
@@ -22,7 +31,7 @@ async def get_element(review_block, xpath):
         name = await review_block.evaluate(f'(element) => {{ return element.querySelector("{xpath}").textContent; }}')
         return name.strip() if name else ''
     except Exception as e:
-        print(f"Ошибка при получении текста элемента: {e}")
+        logger.error(f"Ошибка при получении текста элемента: {e}")
         return ''
 
 
@@ -38,7 +47,7 @@ async def element_click(page, xpath):
         }''', xpath)
         return result
     except Exception as e:
-        print(f"Ошибка при клике по элементу: {e}")
+        logger.error(f"Ошибка при клике по элементу: {e}")
         return False
 
 
@@ -51,133 +60,183 @@ async def el_click(page, path):
         return False
 
 
+async def process_review_block(page, review_block):
+    try:
+        name_element = await review_block.querySelector('span[class^="_16s5yj36"]')
+        name = await page.evaluate('(element) => element.getAttribute("title")', name_element)
+    except Exception as e:
+        name = ''
+    try:
+        date_element = await review_block.querySelector('div[class="_4mwq3d"]')
+        date = await page.evaluate('(element) => element.textContent.trim().split(",")[0]', date_element)
+    except Exception as e:
+        date = ''
+    try:
+        review_text_element = await review_block.querySelector('div[class="_49x36f"] > a')
+        review_text = await page.evaluate('(element) => element.textContent.trim()', review_text_element)
+    except Exception as e:
+        review_text = ''
+
+    ratings = {
+        '50px': 5,
+        '40px': 4,
+        '30px': 3,
+        '20px': 2,
+        '10px': 1
+    }
+
+    rating_elements = await review_block.xpath('.//div[@class="_1fkin5c"]')
+    rating = 0
+    for element in rating_elements:
+        width = await page.evaluate('(element) => getComputedStyle(element).width', element)
+        rating += ratings.get(width, 0)
+
+    return {'Имя': name, 'Отзыв': review_text, 'Оценка': rating, 'Дата': date}
+
+
+async def search_text_on_page(page, search_text):
+    text_exists = await page.evaluate(f'document.body.innerText.includes("{search_text}")')
+    return text_exists
+
+
 async def save_reviews_content(page):
     reviews_content = []
     await page.waitForXPath(pathes.scroll)
     full_elements = await page.xpath(pathes.scroll)
-
-    review_blocks = await page.xpath(
-        '//div[starts-with(@class, "_11gvyqv")]')
+    review_blocks = await page.xpath('//div[starts-with(@class, "_11gvyqv")]')
     total_count = len(review_blocks)
-
-    last_count = 0
+    temp = 0
+    onetemp = False
     last_review_block_index = 0
     while True:
-        temp = 0
-        review_blocks = await page.xpath(
-            '//div[starts-with(@class, "_11gvyqv")]')
-
+        review_blocks = await page.xpath('//div[starts-with(@class, "_11gvyqv")]')
         for idx, review_block in enumerate(review_blocks[last_review_block_index:], start=1):
             temp += 1
             await page.evaluate('(element) => element.scrollIntoView()', review_block)
             await asyncio.sleep(0.1)
-            try:
-                name_element = await review_block.querySelector('span[class^="_16s5yj36"]')
-                name = await page.evaluate('(element) => element.getAttribute("title")', name_element)
-            except Exception as e:
-                name = ''
-            try:
-                date_element = await review_block.querySelector('div[class="_4mwq3d"]')
-                date = await page.evaluate('(element) => element.textContent.trim().split(",")[0]', date_element)
-            except Exception as e:
-                date = ''
-            try:
-                review_text_element = await review_block.querySelector('div[class="_49x36f"] > a')
-                review_text = await page.evaluate('(element) => element.textContent.trim()', review_text_element)
-            except Exception as e:
-                review_text = ''
-
-            ratings = {
-                '50px': 5,
-                '40px': 4,
-                '30px': 3,
-                '20px': 2,
-                '10px': 1
-            }
-
-            rating_elements = await review_block.xpath(
-                './/div[@class="_1fkin5c"]')
-            rating = 0
-            for element in rating_elements:
-                width = await page.evaluate('(element) => getComputedStyle(element).width', element)
-                rating += ratings.get(width, 0)
-
-            reviews_content.append({'Имя': name, 'Отзыв': review_text, 'Оценка': rating, 'Дата': date})
-
+            review_content = await process_review_block(page, review_block)
+            reviews_content.append(review_content)
             last_review_block_index += 1
 
-        if temp == last_count:
-            prev_height = await page.evaluate('(element) => element.scrollHeight', full_elements[
-                0])
-            # eshkere
-            await page.evaluate('(element) => element.scrollTo(0, element.scrollHeight)',
-                                full_elements[0])
-            await asyncio.sleep(0.5)
-            new_height = await page.evaluate('(element) => element.scrollHeight', full_elements[
-                0])
-            if new_height == prev_height:
+        prev_height = await page.evaluate('(element) => element.scrollHeight', full_elements[0])
+        await page.evaluate('(element) => element.scrollTo(0, element.scrollHeight)', full_elements[0])
+        await asyncio.sleep(0.5)
+        new_height = await page.evaluate('(element) => element.scrollHeight', full_elements[0])
+        logger.debug(f"prev_height, new_height, page.xpath")
+        if new_height == prev_height:
+            await element_click(page, '//button[contains(text(), "Загрузить ещё")]')
+            if onetemp:
                 break
             else:
-                last_count = temp
-        else:
-            last_count = temp
+                onetemp = True
 
     return reviews_content, total_count
+
+
+async def process_item(page, item_xpath):
+    element = await page.waitForXPath(item_xpath)
+    await page.evaluate('(element) => { element.scrollIntoView(); }', element)
+    await el_click(page, item_xpath)
+    await asyncio.sleep(0.5)
+    try:
+        address = await get_element_text(page, './/span[@class="_er2xx9"]/a')
+        address = address.strip()
+    except PageError:
+        address = None
+    try:
+        phone_number = await get_element_text(page, '//div[@class="_b0ke8"]/a')
+        phone_number = phone_number.replace('tel:', '')
+    except PageError:
+        phone_number = 'None'
+    if not await element_click(page, pathes.btnreviews1):
+        await element_click(page, pathes.btnreviews2)
+    await asyncio.sleep(2)
+    try:
+        place_rating = await get_element_text(page, '//div[@class="_10fd7sv"]')
+    except PageError:
+        place_rating = 'None'
+    count_rating = await get_element_text(page, pathes.count_rating1) or await get_element_text(page,
+                                                                                                pathes.count_rating2)
+    link = unquote(page.url)
+    reviews_content, _ = await save_reviews_content(page)
+    await save_to_database(reviews_content, link, address, place_rating, phone_number, count_rating)
+
+
+temp_dirs = []
+
+
+async def switch_tabs(pages, browser, tasks):
+    await asyncio.sleep(4)
+    while True:
+        if all(task.done() for task in tasks):
+            break  # Если все задачи завершены, выходим из цикла
+        for i, page in enumerate(pages):
+            await asyncio.sleep(2)
+            await page.bringToFront()
+            logger.info(f"Переключение на вкладку {i + 1}")
+    logger.info(f"Программа успешно выполнена ")
+    global main_tasks_finished
+    main_tasks_finished = True
+    await browser.close()
 
 
 async def main():
     search_query = 'Вкусно и точка'
     url = f'https://2gis.ru/ufa/search/{search_query}'
     browser = await launch(
-        {'executablePath': 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', 'headless': False})
+        {'executablePath': 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', 'headless': False,
+         'args': ['--disable-infobars', '--window-position=0,0']})
 
-    page = await browser.newPage()
-    await page.goto(url)
-    await element_click(page, pathes.cookie_banner)
-    await asyncio.sleep(0.5)
+    pages = await asyncio.gather(*[browser.newPage() for _ in range(3)])
+    queues = [asyncio.Queue() for _ in range(len(pages))]
+
+    for i, (page, queue) in enumerate(zip(pages, queues)):
+        await page.goto(url)
+        await element_click(page, pathes.cookie_banner)
+        await queue.put((page, i))
+    tasks = []
+    for queue in queues:
+        task = asyncio.create_task(process_pages(queue))
+        tasks.append(task)
+    switch_task = asyncio.create_task(switch_tabs(pages, browser, tasks))
+    await asyncio.gather(*tasks, switch_task)
+
+
+async def process_pages(queue):
+    while not queue.empty():
+        page, start_index = await queue.get()
+        await process_page(page, start_index)
+
+
+async def process_page(page, start_index):
     count_all_items = await get_element_text(page, pathes.items_count)
-    pages = round(int(count_all_items) / 12 + 0.5)
-    for _ in range(pages):
+    pages_count = round(int(count_all_items) / 12 + 0.5)
+
+    for _ in range(pages_count):
+        await page.waitForXPath(pathes.main_block)
+
         main_block = await page.waitForXPath(pathes.main_block)
         items = await page.xpath(f'{pathes.main_block}/div')
         count_items = len(items)
-        for item in range(1, count_items + 1):
+
+        max_item_index = count_items - 1
+
+        for item in range(start_index + 1, max_item_index + 1, 3):
             elements = await main_block.xpath(f'div[{item}]')
-            if elements:
-                class_attribute = await page.evaluate('(element) => element.getAttribute("class")', elements[0])
-                if class_attribute:
-                    continue
-            element_xpath = f'/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div/div[{item}]'
-            element = await page.waitForXPath(element_xpath)
-            await page.evaluate('(element) => { element.scrollIntoView(); }', element)
-            await el_click(page, element_xpath)
-            await asyncio.sleep(0.5)
-            try:
-                address = await get_element_text(page, './/span[@class="_er2xx9"]/a')
-                address = address.strip()
-            except PageError:
-                address = None
-            try:
-                phone_number = await get_element_text(page, '//div[@class="_b0ke8"]/a')
-                phone_number = phone_number.replace('tel:', '')
-            except PageError:
-                phone_number = 'None'
-            if not await element_click(page, pathes.btnreviews1):
-                await element_click(page, pathes.btnreviews2)
-            await asyncio.sleep(2)
-            try:
-                place_rating = await get_element_text(page, '//div[@class="_10fd7sv"]')
-            except PageError:
-                place_rating = 'None'
-            count_rating = await get_element_text(page, pathes.count_rating1) or await get_element_text(page,
-                                                                                                        pathes.count_rating2)
-            link = unquote(page.url)
-            reviews_content, _ = await save_reviews_content(page)
-            await save_to_database(reviews_content, link, address, place_rating, phone_number, count_rating)
+            if not elements:
+                break
+            class_attribute = await page.evaluate('(element) => element.getAttribute("class")', elements[0])
+            if class_attribute:
+                continue
+            item_xpath = f'/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div/div[{item}]'
+            logger.info(f"Обрабатывается элемент {item} в потоке {start_index}")
+
+            await process_item(page, item_xpath)
+
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight);')
+
         await element_click(page, pathes.next_page_btn)
         await asyncio.sleep(0.5)
-    await browser.close()
 
 
 async def save_to_database(data, link, address, place_rating, phone_number, count_rating):
@@ -220,7 +279,7 @@ async def save_to_database(data, link, address, place_rating, phone_number, coun
 
         conn.commit()
     except Exception as e:
-        print("Error:", e)
+        logger.error(e)
         conn.rollback()
     finally:
         cursor.close()
@@ -228,4 +287,4 @@ async def save_to_database(data, link, address, place_rating, phone_number, coun
 
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
